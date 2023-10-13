0e98156476c697de8aea223cd9405ae117d26e05
Hebert do Nascimento <hebertn88@gmail.com>
2023-10-13 08:30:15 -0300

    initial commit

---
+++ src/test/java/com/hnasc/orderproducts/OrderProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hnasc.orderproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
